8fa908d0138eb06f101c2dfc973128dd4c34b01f
bensonsin998 <sinbenson0218@gmail.com>
2022-09-24 13:10:18 +0800

    Update

--- src/extension.ts
+++ src/extension.ts
@@ -10,10 +10,18 @@ export async function activate(context: vscode.ExtensionContext) {
 	let token: string = await storageService.getValue("token");
 
 	if(token === undefined)	{
-		vscode.window.showErrorMessage("No ClickUp Token!");
+		vscode.window.showErrorMessage("No ClickUp Token!", ...["Add Click Up Token"]).then((result) => {
+			if(result === undefined) { return; }
+
+			console.log(result);
+		});
 	}
 
 	// Command function
+	vscode.commands.registerCommand("Scrummer.addClickUpToken", () => {
+		vscode.window.showInformationMessage("Adding Click Up token...");
+	});
+
 	vscode.commands.registerCommand("Scrummer.helloWorld", () => {
 		vscode.window.showInformationMessage("Hello World from Scrummer!");
 	});
