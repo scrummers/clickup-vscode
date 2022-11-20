c5b7dfda7d023dd75c19c16c9ab5fc68f11ffb26
WilsonHo0029 <wilson0029_2017@outlook.com>
2022-11-20 18:49:12 +0800

    test: unit-test
    1.add test case: GetDate

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert';
 import { after } from 'mocha';
-
+import { getDate } from '../../util//helper';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
@@ -15,4 +15,9 @@ suite('Extension Test Suite', () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
   });
+ 
+  test('GetDate test', () => {
+    assert.equal('25/11/2022 @8:00am', getDate(1669334400000));
+  });
+
 });
